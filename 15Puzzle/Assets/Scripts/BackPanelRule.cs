1323fdc7481a6c123bcfa22d602c2367228ba368
shimazug <shimazug@marv.jp>
2015-09-18 16:09:06 +0900

    add 空のパネル

---
+++ 15Puzzle/Assets/Scripts/BackPanelRule.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackPanelRule : MonoBehaviour {
+
+	public bool flag;
+
+	// Use this for initialization
+	void Start ()
+	{
+		flag = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D()
+	{
+		flag = true;
+//		this.GetComponent<BoxCollider2D> ().enabled = false;
+	}
+
+	void OnCollisionStay2D()
+	{
+		Debug.Log ("無効！");
+	}
+	
+}
