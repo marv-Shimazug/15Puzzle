c017c0a0758f65b9926574c3ad6765c0956e8ec3
shimazug <shimazug@marv.jp>
2015-09-18 16:07:50 +0900

    add パネルの移動

--- 15Puzzle/Assets/Scripts/GameRule.cs
+++ 15Puzzle/Assets/Scripts/GameRule.cs
@@ -7,30 +7,90 @@ public class GameRule : MonoBehaviour {
 	// パネルの数.
 	const int ALL_PANELS = 16;
 
-	// パネルのプレハブ.
-	[SerializeField]GameObject PanelPrefab;
+	// パネル座標の基準.
+	readonly Vector2 BASE_POS = new Vector2 (-150.0f, 80.0f);
 
-	// カンバス.
+	// シャッフルの回数.
+	const int SHUFFLE_NUM = 1000;
+
+	// UIパネルのプレハブ.
+	[SerializeField]GameObject PanelPrefab;
+	// UI背景パネルのプレハブ.
+	[SerializeField]GameObject BackPanelPrefab;
+
+	// UIカンバス.
 	[SerializeField]GameObject Canvas;
 
-	// パネルオブジェクトの格納.
+	// UIパネルオブジェクトの格納.
 	GameObject[] PanelObjects = new GameObject[ALL_PANELS]; 
+	GameObject[] BackObjects = new GameObject[ALL_PANELS];
 
 	// パネル用意.
 	int[] Panel= new int[ALL_PANELS];
 
-	// Use this for initialization
+	// 選択中のパネル番号.
+	public int SelectedPanel;
+
+	// マウスカーソルの下にあるオブジェクト.
+	GameObject UnderMouseObj;
+
+	// 初期化.
 	void Start () 
 	{
+		SelectedPanel = -1;
+
+		// 描画順の関係で、同じfor文で回せない.
 		for (int i = 0; i < ALL_PANELS; i++) 
 		{
-			CreatePanelObjects( i );
+			// ベースとなる.
+			UICreatePanelObjects( i, BackPanelPrefab, BackObjects, true );
+		}
+		for (int i = 0; i < ALL_PANELS - 1; i++) 
+		{
+
+			// 移動のための.
+			UICreatePanelObjects( i, PanelPrefab, PanelObjects, false );
 		}
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	// 更新.
+	void Update () 
+	{
+		int i, x, y = 0;
+		x = 0;
+
+		/*
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			Debug.Log("選択中パネル：" + SelectedPanel);
+			x = (int)PanelObjects[SelectedPanel].GetComponent<RectTransform>().position.x;
+			y = (int)PanelObjects[SelectedPanel].GetComponent<RectTransform>().position.y;
+		}
+		*/
+		int selecterpanel = -1;
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			// 選択したパネル番号取得.
+			selecterpanel = int.Parse(this.GetComponentInChildren<Text>().text);
+		}
+
+		int pn = y * 4 + x;
+		if (x > 0 && 15 == Panel [pn - 1]) Swap(pn - 1, pn);
+		if (x < 3 && 15 == Panel [pn + 1]) Swap (pn, pn + 1);
+		if (y > 0 && 15 == Panel [pn - 4]) Swap (pn - 4, pn);
+		if (y < 3 && 15 == Panel [pn + 4]) Swap (pn, pn + 4);
+
+		// ゲーム終了判定.
+		for (i = 0; i < ALL_PANELS; i++) 
+		{
+			if(Panel[i] != i)break;
+			if(ALL_PANELS == i)
+			{
+				// クリア.
+				Debug.Log("クリア！");
+			}
+		}
+
 	}
 
 	// ２つのパネルの位置を入れ替える.
@@ -41,12 +101,78 @@ public class GameRule : MonoBehaviour {
 		Panel [j] = tmp;
 	}
 
-	// パネル生成.
-	void CreatePanelObjects(int panel)
+	// パズル生成.
+	void SetPuzzle()
 	{
-		PanelObjects[panel] = Instantiate (PanelPrefab, new Vector3((float)panel * 30.0f, 50.0f, 0.0f), Quaternion.identity) as GameObject;
-		PanelObjects[panel].GetComponentInChildren<Text>().text = panel.ToString();
-		PanelObjects [panel].transform.parent = Canvas.transform;
+		for (int i = 0; i < ALL_PANELS; i++) 
+		{
+			Panel[i] = i;
+		}
+	}
+
+	// シャッフル.
+	void PanelShuffle()
+	{
+		int i, pn = 0;
+		for(i = 0; i < SHUFFLE_NUM; i++)
+		{
+			for(int j = 0; j < ALL_PANELS; j++)
+			{
+				// 空きパネルをpnに格納.
+				if(ALL_PANELS - 1 == Panel[j])
+				{
+					pn = j;
+				}
+				// 空きパネル座標.
+				int x = pn % 4;
+				int y = pn / 4;
+				// 空きパネルと入れ替える方向をランダムで決める.
+				int dir = Random.Range(1, 9999) % 4;
+				switch(dir)
+				{
+				case 0: if(x > 0) Swap(pn - 1, pn); break;
+				case 1: if(x < 3) Swap(pn, pn + 1); break;
+				case 2: if(y > 0) Swap(pn, pn + 4); break;
+				case 3: if(y < 3) Swap(pn, pn + 4); break;
+				}
+			}
+		}
+	}
+
+	// パネルUI生成.
+	void UICreatePanelObjects(int panel, GameObject prefab, GameObject[]  objects, bool back)
+	{
+		// インスタンス作成.
+		objects[panel] = Instantiate (prefab) as GameObject;
+
+		// 番号表示.
+		if (false == back) 
+		{
+			PanelObjects [panel].GetComponentInChildren<Text> ().text = panel.ToString ();
+		}
+
+		// 親オブジェクト設定.
+		objects [panel].transform.parent = Canvas.transform;
+
+		// 座標設定.
+		Vector3 Pos = Vector3.zero;
+		Pos.x = (float)(panel % 4);
+		Pos.y = (float)(panel / 4);
+		if (true == back) 
+		{
+			Pos.z = -1.0f;
+		} 
+		else 
+		{
+			Pos.z = 0.0f;
+		}
+		objects[panel].GetComponent<RectTransform>().localPosition = new Vector3(Pos.x * 50.0f + BASE_POS.x, BASE_POS.y - (Pos.y * 50.0f), Pos.z);
+	}
+
+	// 空パネル生成.
+	void EmptyPanel(int panel, GameObject prefab)
+	{
+
 	}
 
 }
